b3cac1e173e91b7b3d75e9fe8f6727aadf8de5d6
Vinod Patidar <vinodpatidar0808@gmail.com>
2024-06-30 17:35:42 +0530

    use port from .env

--- server/index.js
+++ server/index.js
@@ -1,10 +1,10 @@
 const express = require('express');
 const ws = require('ws');
+require("dotenv").config();
 
 const app = express();
 
 const port = process.env.PORT || 8080;
-
 
 const httpServer = app.listen(port, () => {
   console.log(`Server running on port ${port}`);
